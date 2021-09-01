ca3786997a4ca2806574bc5864f2df454bff54ed
Pabna97 <66499739+Pabna97@users.noreply.github.com>
2021-09-01 20:26:35 +0600

    Create Locl.js

---
+++ Locl.js
@@ -0,0 +1,9 @@
+<SCRIPT LANGUAGE="JavaScript">
+<!-- Begin
+var password = 'password'
+password=prompt('Please enter the password to enter your bank acount  :','');
+if (password !='arisha@@') {
+location.href='URL';
+}
+// End -->
+</SCRIPT>    
