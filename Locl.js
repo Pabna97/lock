690f1e66b630231a273657f22acbe6aad01c8f50
Pabna97 <66499739+Pabna97@users.noreply.github.com>
2021-09-02 15:11:17 +0600

    Lock.js

--- Locl.js
+++ Locl.js
@@ -1,9 +1,9 @@
-<SCRIPT LANGUAGE="JavaScript">
+
 <!-- Begin
 var password = 'password'
 password=prompt('Please enter the password to enter your bank acount  :','');
-if (password !='arisha@@') {
+if (password !='ari@@') {
 location.href='URL';
 }
 // End -->
-</SCRIPT>    
+
